c3854419820a5617bb7804d29bb15ad03735f8c0
xjdlb <xjd.binlee@qq.com>
2019-12-27 22:56:34 +0800

    [add]init repos.

---
+++ src/main/java/com/spark/core/AppMain.java
@@ -0,0 +1,14 @@
+package com.spark.core;
+
+/**
+ * 记录spark部分实例
+ *
+ * @Author: leebin
+ * @Date: 2019/12/27 10:36 下午
+ * @Description:
+ */
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("keep some spark demos");
+    }
+}
---
+++ src/main/java/com/spark/ml/MlAppMain.java
@@ -0,0 +1,9 @@
+package com.spark.ml;
+
+/**
+ * @Author: leebin
+ * @Date: 2019/12/27 10:45 下午
+ * @Description:
+ */
+public class MlAppMain {
+}
